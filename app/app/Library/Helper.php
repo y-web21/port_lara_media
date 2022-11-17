c3560a44c453681be110217f88b7274fc92c8ac4
y-web21 <51986098+y-web21@users.noreply.github.com>
2022-11-17 23:14:17 +0900

    自作ヘルパをファサードに追加

---
+++ app/app/Library/Helper.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Library;
+
+class Helper
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * 文字列を指定の長さまでに制限してエリプシスをつけて返します。
+     * エリプシスの長さは制限に含まれません。
+     *
+     * @param string $value 対象文字列
+     * @param integer $limit optional 表示文字数制限の指定
+     * @param string $end optional エリプシス文字列の指定
+     * @return void
+     */
+    public static function strLimit(string $value, int $limit = 100, string $end = '...')
+    {
+        if (mb_strlen($value, 'UTF-8') <= $limit) {
+            return $value;
+        }
+        return rtrim(mb_substr($value, 0, $limit, 'UTF-8')) . $end;
+    }
+
+    /**
+     * @param model $model
+     * @return array<int, string>
+     */
+    public static function getTableColumnName($model)
+    {
+        $columns = $model->getConnection()->getSchemaBuilder()->getColumnListing($model->getTable());
+        return $columns;
+    }
+
+}
