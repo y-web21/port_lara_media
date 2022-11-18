4cde37ce01efcee47843edb7440ea09d56c95062
y-web21 <51986098+y-web21@users.noreply.github.com>
2022-11-18 19:16:47 +0900

    sample test 追加

--- app/README.md
+++ app/README.md
@@ -1,3 +1,14 @@
+## test
+
+```php
+php artisan make:test <test_name>
+
+# 実行。どちらのやり方でも phpunit のオプションが使用可能 
+./vendor/bin/phpunit
+# or
+php artisan test
+```
+
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
 
 <p align="center">
---
+++ app/tests/Feature/RouteTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RouteTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function test_home()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * test_basic_access
+     *
+     * @dataProvider getAccessDataProvider
+     * @param  string $page
+     * @return void
+     */
+    public function test_basic_access(string $page)
+    {
+        $response = $this->get($page);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * getAccessDataProvider
+     *
+     * @return array
+     */
+    public function getAccessDataProvider(): array
+    {
+        return [
+            ['home'],
+            ['/welcome'],
+        ];
+    }
+}
