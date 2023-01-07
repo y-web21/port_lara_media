f1f79de5ec79937298f2425cadaa48d39be9c7c7
y-web21 <51986098+y-web21@users.noreply.github.com>
2023-01-07 12:08:49 +0000

    [#56] ユーザー登録をナビに追加

--- app/app/Consts/Navigation.php
+++ app/app/Consts/Navigation.php
@@ -9,21 +9,28 @@ namespace App\Consts;
 class Navigation
 {
     const ACTION_INDEX  = 'index';
-    const HOME    = 'home';
-    const ARTICLE = 'article';
-    const ABOUT   = 'about';
-    const LOGIN   = 'login';
-    const INDEX   = 'home';
+    const ACTION_CREATE = 'create';
+    const HOME          = 'home';
+    const ARTICLE       = 'article';
+    const ABOUT         = 'about';
+    const LOGIN         = 'login';
+    const REGISTER      = 'register';
+    const INDEX         = 'home';
+    const MEMBER        = 'member.home';
 
     /**
-     * html 文字列 => Action
+     * Route => Name
      */
     const LIST_GLOBAL = [
-        self::HOME => self::ACTION_INDEX,
-        self::ARTICLE => self::ACTION_INDEX,
-        self::ABOUT => self::ACTION_INDEX,
+        self::HOME . '.' . self::ACTION_INDEX => self::HOME,
+        self::ARTICLE . '.' . self::ACTION_INDEX => self::ARTICLE,
+        self::ABOUT . '.' . self::ACTION_INDEX => self::ABOUT,
     ];
     const LIST_GLOBAL_LOGIN = [
-        self::LOGIN => self::ACTION_INDEX
+        self::LOGIN => self::LOGIN,
+        self::REGISTER => self::REGISTER,
+    ];
+    const LIST_GLOBAL_LOGIN_AUTH = [
+        self::MEMBER => 'My page'
     ];
 }
