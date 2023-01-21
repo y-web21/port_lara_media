46d03c0976fa9dccc2a6b1ce5fe211c2229ecd2b
y-web21 <51986098+y-web21@users.noreply.github.com>
2023-01-21 11:48:01 +0900

    [#56] パスワードルールの追加とテストの追加

--- app/tests/Feature/RegistrationTest.php
+++ app/tests/Feature/RegistrationTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Providers\RouteServiceProvider;
+use DivisionByZeroError;
+use Exception;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Fortify\Features;
 use Laravel\Jetstream\Jetstream;
@@ -14,7 +16,7 @@ class RegistrationTest extends TestCase
 
     public function test_registration_screen_can_be_rendered()
     {
-        if (! Features::enabled(Features::registration())) {
+        if (!Features::enabled(Features::registration())) {
             return $this->markTestSkipped('Registration support is not enabled.');
         }
 
@@ -36,19 +38,52 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register()
     {
-        if (! Features::enabled(Features::registration())) {
+        if (!Features::enabled(Features::registration())) {
             return $this->markTestSkipped('Registration support is not enabled.');
         }
 
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
+            'password' => 'Password-0',
+            'password_confirmation' => 'Password-0',
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
         ]);
 
         $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
+    /**
+     * @test
+     * @dataProvider invalid_register_login_password_provider
+     */
+    public function パスワード登録失敗ルールの検証(string $password, string $confirmPass=null)
+    {
+        if (!Features::enabled(Features::registration())) {
+            return $this->markTestSkipped('Registration support is not enabled.');
+        }
+
+        $response = $this->post('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => $password,
+            'password_confirmation' => $confirmPass ?? $password,
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+        $this->assertFalse($this->isAuthenticated(), 'User creation successed...');
+    }
+
+    public function invalid_register_login_password_provider()
+    {
+        return [
+            'lowercase' => ['password'],
+            'uppercase' => ['PASSWORD'],
+            'number' => ['01234567'],
+            'symbol' => ['-_@#-_@#'],
+            'withoutSymbol' => ['PassWord'],
+            'char7' => ['@7Passw'],
+            'different' => ['Password-0', 'Password_0'],
+        ];
     }
 }
